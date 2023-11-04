703f1a555c094c436a3019d16f2c0654f07fa2f1
batuhankendirli <batuhankndrl@gmail.com>
2023-11-05 02:46:08 +0300

    added footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,14 @@
+const year = new Date().getFullYear();
+
+const Footer = () => {
+  return (
+    <footer className="flex items-center text-base justify-center mt-8 gap-1 text-color-fourth border-t-2 border-color-fourth border-opacity-10 pt-8">
+      <a href="https://batuhankendirli.netlify.app/" target={'_blank'} className="text-lg font-medium footer-link">
+        Batuhan Kendirli
+      </a>
+      <span>- {year}</span>
+    </footer>
+  );
+};
+
+export default Footer;
