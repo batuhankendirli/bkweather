59731a497e6f443a823e67d03875d93b245fa8f7
batuhankendirli <batuhankndrl@gmail.com>
2023-11-05 21:22:32 +0300

    added metadata for weather pages

--- app/city/[id]/page.tsx
+++ app/city/[id]/page.tsx
@@ -5,9 +5,22 @@ import Nav from '@/app/components/Nav';
 import Graph from '@/app/components/Graph';
 import Conditions from '@/app/components/Conditions';
 import CityInfo from '@/app/components/CityInfo';
+import { Metadata } from 'next';
 
 interface Params {
   params: { id: number };
+}
+
+type Props = {
+  params: { id: string };
+};
+
+export function generateMetadata({ params }: Props): Metadata {
+  const id = params.id;
+
+  return {
+    title: `${cities[Number(id) - 1].name} Hava Durumu | Batuhan Kendirli`,
+  };
 }
 
 const CityDetail = async ({ params }: Params) => {
@@ -15,13 +28,13 @@ const CityDetail = async ({ params }: Params) => {
   const cityData = await getWeatherData(latitude, longitude);
 
   return (
-    <div>
+    <>
       <Nav />
       <CityInfo data={cityData} />
       <Conditions />
       <Graph data={cityData} />
       <CardsWrapper data={cityData} />
-    </div>
+    </>
   );
 };
 
